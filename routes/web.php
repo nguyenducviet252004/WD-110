b95a834bcd663b8c997a64cf59acdecb7931fe69
trngochuy969@gmail.com <trngochuy969@gmail.com>
2025-06-25 22:29:42 +0700

    update admin category sau reset

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('admin/categories')->name('admin.categories.')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('index');
+    Route::get('/create', [CategoryController::class, 'create'])->name('create');
+    Route::post('/', [CategoryController::class, 'store'])->name('store');
+    Route::get('/{category:slug}/edit', [CategoryController::class, 'edit'])->name('edit');
+    Route::put('/{category:slug}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('/{category:slug}', [CategoryController::class, 'destroy'])->name('destroy');
+    Route::get('/{category:slug}', [CategoryController::class, 'show'])->name('show');
+    // new
 });
