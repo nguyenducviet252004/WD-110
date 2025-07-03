9763724408da866a6a460a50e0e5c967c0cf5e71
Phongtd22 <truongduyphong18@gmail.com>
2025-07-03 17:28:38 +0700

    API Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -40,6 +40,11 @@ class Product extends Model
         return $this->hasMany(ProductVariant::class);
     }
 
+      public function galleries()
+    {
+        return $this->hasMany(ProductGallery::class);
+    }
+    
     // Accessor để trả về URL đầy đủ cho thumb_image
     public function getThumbImageAttribute($value)
     {
--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -38,4 +38,4 @@ class Voucher extends Model
 
 }
 
-}
+
