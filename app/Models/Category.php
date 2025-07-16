963991776837f172d03dfcbc9714646f6c467268
hotrung662004 <hotrung662004@gmail.com>
2025-07-16 22:22:55 +0700

    shipadress,cate

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'is_active',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }
