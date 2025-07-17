c6c7f68787b999e4747280f4dbc99a439270307b
nguyenducviet252004 <nguyenducviet252004@gmail.com>
2025-07-17 09:10:29 +0700

    sua accountcontroller admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\ColorController;
 use App\Http\Controllers\SizeController;
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AccountController::class)->group(function () {
+ // Đăng ký
+    Route::get('register', 'register')->name('register.form');
+    Route::post('register', 'register_')->name('register');
+     // Đăng nhập
+    Route::get('login', 'login')->name('login.form');
+    Route::post('login', 'login_')->name('login');
+// Quên mật khẩu
+    Route::get('password/forgot', 'rspassword')->name('password.forgot.form');
+    Route::post('password/forgot', 'rspassword_')->name('password.forgot');
+    // Đặt lại mật khẩu
+    Route::get('password/reset/{token}', 'updatepassword')->name('password.reset');
+    Route::post('password/reset', 'updatepassword_')->name('password.update');
+        // Xác thực email
+    Route::get('/verify', 'verify')->name('verify')->middleware('auth');
+    Route::get('/verify/{id}/{hash}', 'verifydone')->name('verification.verify');
+
+    // Đăng xuất
+    Route::post('logout', 'logout')->name('logout');
 });
 
 // Route cho Admin
