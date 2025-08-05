5a45b8feb5461e4ecafe082245a439f9f7e30f0b
Phongtd22 <truongduyphong18@gmail.com>
2025-08-05 22:09:00 +0700

    UserController.updateUser

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -63,5 +65,62 @@ class UserController extends Controller
         $addresses = $user->shipAddresses;
 
         return view('user.update', compact('user', 'addresses'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $request->validate([
+            'fullname' => 'nullable|string|max:255',
+            'birth_day' => 'nullable|date',
+            'phone' => 'nullable|string|max:15',
+            'email' => 'nullable|email|unique:users,email,' . $user->id,
+            'address' => 'nullable|string|max:255',
+            'avatar' => 'nullable|image',
+        ]);
+
+        $user->fullname = $request->input('fullname');
+        $user->birth_day = $request->input('birth_day');
+        $user->phone = $request->input('phone');
+        $user->email = $request->input('email');
+        $user->address = $request->input('address');
+
+        if ($request->hasFile('avatar')) {
+            // Xóa ảnh cũ nếu tồn tại
+            if ($user->avatar && Storage::exists($user->avatar)) {
+                Storage::delete($user->avatar);
+            }
+            // Lưu ảnh mới và cập nhật đường dẫn vào cột avatar
+            $user['avatar'] = Storage::put('UserAvatar', $request->file('avatar'));
+        }
+
+        $addressId = $request->input('address_id'); // Nhận ID địa chỉ từ request
+        if ($addressId) {
+            DB::transaction(function () use ($user, $addressId) {
+                // Đặt tất cả các địa chỉ khác về 0
+                   /**
+                 * @var User $user
+                 */
+                  /**
+                 * @var Ship_address $user
+                 */
+                $user->shipAddresses()->update(['is_default' => 0]);
+
+                // Cập nhật địa chỉ được chọn thành mặc định
+                $address = $user->shipAddresses()->find($addressId);
+                if ($address) {
+                    $address->is_default = 1; // Đặt is_default thành 1
+                    $address->save(); // Lưu lại thay đổi
+                }
+            });
+        }
+        
+           /**
+             * @var User $user
+             */
+        $user->save();
+
+        return redirect()->back()->with('success', 'Thông tin tài khoản đã được cập nhật thành công.');
     }
 }
