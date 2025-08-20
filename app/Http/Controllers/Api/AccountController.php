f4a79702d84b2ebde74b130595bbf86ddfc8b0cd
Phongtd22 <truongduyphong18@gmail.com>
2025-08-20 16:17:45 +0700

    Api-Account-register

---
+++ app/Http/Controllers/Api/AccountController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Throwable;
+
+class AccountController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate incoming request
+        $request->validate([
+            'email' => ['required', 'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,4}$/', 'unique:users,email'],
+            'password' => 'required|string|min:6',
+            'confirmPassword' => 'required|same:password', // Ensure confirm_password matches password
+        ]);
+    
+        try {
+            // Prepare user data
+            $userData = [
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),  // Hash the password
+                'role' => $request->filled('role') ? $request->input('role') : 0,  // Default role is 0
+            ];
+    
+            // Create user
+            $user = User::create($userData);
+    
+            // Return success response
+            return response()->json([
+                'status' => true,
+                'message' => 'Đăng kí thành công',
+                'data' => [
+                    'email' => $user->email,
+                ]
+            ], 200);
+    
+        } catch (Throwable $e) {
+            // Handle error
+            return back()->with('error', $e->getMessage());
+        }
+    }
+
+    
+}
