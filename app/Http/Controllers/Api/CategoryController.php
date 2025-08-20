4f96d0c6ebab26f3958da1c0e2cf80e8bb271455
Hồ Trung <hotrung662004@gmail.com>
2025-08-20 21:19:43 +0700

    CategoryController

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,3 +7,27 @@ use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $categories = Category::where('is_active', 1)->get();
+            return response()->json($categories);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Không thể lấy danh sách categories.'], 500);
+        }
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+       
+    }
